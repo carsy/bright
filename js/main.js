96b13cd5c3e0dbc75dccfa1f5f644e18b7d93276
Zé Bateira <jlageb+github@gmail.com>
2013-08-31 17:21:18 +0100

    Menus Updated. Fixed menu on top. colors for first two sections change when scrolling. (TODO: when section clicked, slide and change color.)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+/*
+var t = $("#anchor-point").offset().top;
 
+$(document).scroll(function(){
+    if($(this).scrollTop() > t)
+    {   
+        $('#voice2').css({"border-bottom":"2px solid #f4f5f8"});
+        $('#voice3').css({"border-bottom":"2px solid #2e375b"});
+    }
+});
+*/
+
+$(document).ready(function() {
+	var le_header = $('#le_header');
+
+	var marker = $('#what_header').offset().top;
+	var originalBG = le_header.css('background');
+
+	$(document).scroll(function() {
+		if( $(this).scrollTop() > marker ) {
+			le_header.css("background", $('#what_header').css("background"));
+		}
+		else if( $(this).scrollTop() < marker ) {
+			le_header.css("background", originalBG);
+		}
+	});
+});
