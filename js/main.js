73631eecddc8473d42000c4f74b036f342d70e45
Zé Bateira <jlageb+github@gmail.com>
2013-09-03 18:12:37 +0100

    Section's transitions updated. Animations added.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,74 @@
 $(document).ready(function() {
-	var le_header = $('#le_header');
+	var home_header = $('#home_header');
+	var what_header = $('#what_header');
+	var who_header = $('#who_header');
+	var news_header = $('#news_header');
+	var partners_header = $('#partners_header');
 
-	var headers = {
-		'le_header': { marker: le_header.offset().top, background: le_header.css('background-image'), elem: le_header, link: $('a[href=#home]') },
-		'what_header': { marker: $('#what_header').offset().top, background: $('#what_header').css('background-image'), elem: $('what_header'), link: $('a[href=#what]') },
-		'who_header': { marker: $('#who_header').offset().top, background: $('#who_header').css('background-image'), elem: $('#who_header'), link: $('a[href=#who]') },
-		'news_header': { marker: $('#news_header').offset().top, background: $('#news_header').css('background-image'), elem: $('#news_header'), link: $('a[href=#news]') },
-		'partners_header': { marker: $('#partners_header').offset().top, background: $('#partners_header').css('background-image'), elem: $('#partners_header'), link: $('a[href=#partners]') }
+	var headers = [
+		{
+			id: 'home_header',
+			marker: home_header.offset().top,
+			background: home_header.css('background-image'),
+			elem: home_header,
+		},
+		{
+			id: 'what_header',
+			marker: what_header.offset().top,
+			background: what_header.css('background-image'),
+			elem: what_header,
+		},
+		{
+			id: 'who_header',
+			marker: who_header.offset().top,
+			background: who_header.css('background-image'),
+			elem: who_header,
+		},
+		{
+			id: 'news_header',
+			marker: news_header.offset().top, 
+			background: news_header.css('background-image'), 
+			elem: news_header, 
+		},
+		{ 
+			id: 'partners_header',
+			marker: partners_header.offset().top, 
+			background: partners_header.css('background-image'), 
+			elem: partners_header, 
+		}
+	];
+
+	var curMarker = -1
+	for(var i = 0; i < headers.length; ++i) {
+		if( home_header.offset().top >= headers[i].marker )
+			curMarker++;
 	}
 
-	$(document).scroll(function() {
+	$(window).scroll(function() {
+		var headerTop = $('#home_header').offset().top;
 
-		for(var key in headers) {
-			if( $(this).scrollTop() >= headers[key].marker ) {
-				le_header.css('background', headers[key].background);
-			}
+		curMarker = -1;
+		for(var i = 0; i < headers.length; ++i) {
+			if( headerTop >= headers[i].marker )
+				curMarker++;
 		}
+		$('#home_header').css('background', headers[curMarker].background);
+		
 	});
+
 	
-	window.onresize = function() {
+	$('a[href=#home], a[href=#what], a[href=#who], a[href=#news], a[href=#partners]').click(function() {
+		var href = $(this).attr('href');
+		var top2scroll2 = (href == '#home' ? 0 : $(href + '_header').offset().top);
+		$('html, body').animate({
+ 			scrollTop: top2scroll2+1,
+		}, 1000, function () {
+			$('#home_header').css('background', headers[curMarker].background);
+		});
+	})
+
+	
+	$(window).resize(function() {
 		var dummy_right = $('section#what .dummy-background-content-right');
 		dummy_right.css('width', $('section#what div.content-right').outerWidth());
 		var dummy_bg = $('section#what .content-title .dummy-background-content');
@@ -26,7 +76,7 @@ $(document).ready(function() {
 
 		var content_right = $('section#what section.section-content');
 		dummy_bg.css('width', parseInt(content_right.css('margin-right'), 10) + parseInt(content_right.css('padding-right'), 10));
-	};
+	});
 
-	window.onresize();
+	$(window).resize();
 });
