c0d848cff7745e9135b0c435bcaa89bb30b9da6e
Zé Bateira <jlageb+github@gmail.com>
2013-09-06 00:11:37 +0100

    expander on project's section started.

--- js/main.js
+++ js/main.js
@@ -5,28 +5,28 @@ $(document).ready(function() {
 	var headers = [
 		{
 			selector: '#home_header',
-			background1: home_header_bg1.css('background'),
-			background2: home_header_bg2.css('background')
+			background1: home_header_bg1.css('background-color'),
+			background2: home_header_bg2.css('background-color')
 		},
 		{
 			selector: '#what_header',
-			background1: $('#what_header > .dummy-background-1').css('background'),
-			background2: $('#what_header > .dummy-background-2').css('background')
+			background1: $('#what_header > .dummy-background-1').css('background-color'),
+			background2: $('#what_header > .dummy-background-2').css('background-color')
 		},
 		{
 			selector: '#who_header',
-			background1: $('#who_header > .dummy-background-1').css('background'),
-			background2: $('#who_header > .dummy-background-2').css('background')
+			background1: $('#who_header > .dummy-background-1').css('background-color'),
+			background2: $('#who_header > .dummy-background-2').css('background-color')
 		},
 		{
 			selector: '#news_header',
-			background1: $('#news_header > .dummy-background-1').css('background'),
-			background2: $('#news_header > .dummy-background-2').css('background')
+			background1: $('#news_header > .dummy-background-1').css('background-color'),
+			background2: $('#news_header > .dummy-background-2').css('background-color')
 		},
 		{ 
 			selector: '#partners_header',
-			background1: $('#partners_header > .dummy-background-1').css('background'),
-			background2: $('#partners_header > .dummy-background-2').css('background')
+			background1: $('#partners_header > .dummy-background-1').css('background-color'),
+			background2: $('#partners_header > .dummy-background-2').css('background-color')
 		}
 	];
 
@@ -45,22 +45,49 @@ $(document).ready(function() {
 			if( headerTop >= $(headers[i].selector).offset().top )
 				curMarker++;
 		}
-		$('#home_header > .dummy-background-1').css('background', headers[curMarker].background1);
-		$('#home_header > .dummy-background-2').css('background', headers[curMarker].background2);
+		$('#home_header > .dummy-background-1').css('background-color', headers[curMarker].background1);
+		$('#home_header > .dummy-background-2').css('background-color', headers[curMarker].background2);
 		
 	});
 
-	
+	// Adds animation for section transitions
 	$('a[href=#home], a[href=#what], a[href=#who], a[href=#news], a[href=#partners]').click(function() {
 		var href = $(this).attr('href');
 		var top2scroll2 = (href == '#home' ? 0 : $(href + '_header').offset().top);
+
 		stop();
+
 		$('html, body').animate({
  			scrollTop: top2scroll2+1,
 		}, 500, function () {
-			$('#home_header > .dummy-background-1').css('background', headers[curMarker].background1);
-			$('#home_header > .dummy-background-2').css('background', headers[curMarker].background2);
+			$('#home_header > .dummy-background-1').css('background-color', headers[curMarker].background1);
+			$('#home_header > .dummy-background-2').css('background-color', headers[curMarker].background2);
 		});
-	})
+	});
 
+	$('.expander').click(function() {
+		if( $(this).html() == "+" )
+			$(this).html(">");
+		else
+			$(this).html("+");
+	});
+
+	/*
+
+	var context = document.getElementById('what_canvas').getContext('2d');
+	var x0 = 25, y0 = 10,
+		xd = 7, yd = 7;
+
+	context.beginPath();
+	context.moveTo(x0, y0);
+	for(i = 0; i < 6; ++i) {
+		context.lineTo((x0 += xd), y0 - yd);
+		context.lineTo((x0 += xd), y0);
+	}
+	context.lineWidth = 2;
+
+	context.strokeStyle = 'white';
+	context.stroke();
+
+	*/
 });
