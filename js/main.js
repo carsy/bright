cf25db4a1c42ed571f3542d223f0e7e3fca1305d
Zé Bateira <jlageb+github@gmail.com>
2013-09-12 16:01:07 +0100

    Section "news" updated. News body added.

--- js/main.js
+++ js/main.js
@@ -68,29 +68,58 @@ $(document).ready(function() {
 
 		event.preventDefault();
 	});
+	var what_section = $('#what');
 
-	$('.expander').click(function(event) {
-		$('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]').slideToggle('slow');
+	$('.expander', what_section).click(function(event) {
+		$('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]', what_section)
+			.slideToggle('slow');
 	
 		if( $(this).children().text() == "+" ) {
 			$(this).children().text(">");
 
-			$('.expander').not($(this)).each(function() {
+			$('.expander', what_section).not($(this)).each(function() {
 				if ($(this).children().text() == "+")
 					return;
 
 				$(this).children().text("+");
 
 
-				$('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]')
-					.slideToggle('slow')
+				$('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]', what_section)
+					.slideToggle('fast')
 				});
 		}
 		else {
 			$(this).children().text("+");
 		}
 
-		history.replaceState(null, '', $(this).id()); // FIXME
+		// history.replaceState(null, '', $(this).id()); // FIXME
+		event.preventDefault();
+	});
+
+	var news_section = $('#news');
+	$('.expander', news_section).click(function(event) {
+		$('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]', news_section)
+			.slideToggle('slow');
+	
+		if( $(this).children().text() == "+" ) {
+			$(this).children().text(">");
+
+			$('.expander', news_section).not($(this)).each(function() {
+				if ($(this).children().text() == "+")
+					return;
+
+				$(this).children().text("+");
+
+
+				$('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]', news_section)
+					.slideToggle('fast')
+				});
+		}
+		else {
+			$(this).children().text("+");
+		}
+
+		// history.replaceState(null, '', $(this).id()); // FIXME
 		event.preventDefault();
 	});
 
