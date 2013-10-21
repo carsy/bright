e8989951efcb7cdddd18e8726782a3d0a78fcc22
Zé Bateira <jlageb+github@gmail.com>
2013-10-21 14:54:43 +0100

    Started using gruntjs and npm.

---
+++ Gruntfile.js
@@ -0,0 +1,123 @@
+/*global module:false*/
+module.exports = function (grunt) {
+
+  // show elapsed time at the end
+  require('time-grunt')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    // configurable paths
+    app: {
+      js: 'js',
+      images: 'img',
+      fonts: 'fonts',
+      css: 'css',
+      sass: 'css'
+    },
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      all: [
+        'Gruntfile.js'
+        // '<%= app.js %>/*.js'
+      ],
+      gruntfile: {
+        src: 'Gruntfile.js'
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      compass: {
+        files: ['<%= app.sass %>/*.{scss,sass}'],
+        tasks: ['compass:server', 'autoprefixer']
+      },
+      styles: {
+        files: ['<%= app.css %>/*.css'],
+        tasks: ['autoprefixer']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          'index.html',
+          '<%= app.css %>/*.css',
+          '<%= app.js %>/*.js',
+          '<%= app.images %>/*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: ['.']
+        }
+      }
+    },
+    compass: {
+      options: {
+        sassDir: '<%= app.sass %>',
+        cssDir: '<%= app.css %>',
+        imagesDir: '<%= app.images %>',
+        javascriptDir: '<%= app.js %>',
+        fontsDir: '<%= app.fonts %>'
+      },
+      server: {
+        options: {
+          debugInfo: true
+        }
+      }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 1 version']
+      }
+    }
+  });
+
+
+  grunt.registerTask('server', function () {
+    grunt.task.run([
+      'autoprefixer',
+      'connect:livereload',
+      'compass:server',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('build', function () {
+    grunt.task.run([]); // TODO: minify all the things: scripts, css, html, images
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'autoprefixer', 'compass']);
+
+};
--- js/main.js
+++ js/main.js
@@ -1,225 +1,229 @@
-$(document).ready(function() {
-	var home_header_bg1 = $('#home_header > .dummy-background-1');
-	var home_header_bg2 = $('#home_header > .dummy-background-2');
+'use strict';
 
-	var headers = [
-		{
-			selector: '#home_header',
-			background1: home_header_bg1.css('background-color'),
-			background2: home_header_bg2.css('background-color')
-		},
-		{
-			selector: '#what_header',
-			background1: $('#what_header > .dummy-background-1').css('background-color'),
-			background2: $('#what_header > .dummy-background-2').css('background-color')
-		},
-		{
-			selector: '#who_header',
-			background1: $('#who_header > .dummy-background-1').css('background-color'),
-			background2: $('#who_header > .dummy-background-2').css('background-color')
-		},
-		{
-			selector: '#news_header',
-			background1: $('#news_header > .dummy-background-1').css('background-color'),
-			background2: $('#news_header > .dummy-background-2').css('background-color')
-		},
-		{ 
-			selector: '#partners_header',
-			background1: $('#partners_header > .dummy-background-1').css('background-color'),
-			background2: $('#partners_header > .dummy-background-2').css('background-color')
-		},
-		{
-			selector: '#contacts_header',
-			background1: $('#contacts_header > .dummy-background-1').css('background-color'),
-			background2: $('#contacts_header > .dummy-background-2').css('background-color')
-		}
-	];
+/* global $:false */
+/* global google:false */
 
-	var home_header = $('#home_header');
-	var curMarker = -1;
-	for(var i = 0; i < headers.length; ++i) {
-		if( home_header.offset().top >= $(headers[i].selector).offset().top )
-			curMarker++;
-	}
+$(document).ready(function () {
+  var home_header_bg1 = $('#home_header > .dummy-background-1');
+  var home_header_bg2 = $('#home_header > .dummy-background-2');
 
-	$(window).scroll(function() {
-		var headerTop = $('#home_header').offset().top;
+  var headers = [{
+    selector: '#home_header',
+    background1: home_header_bg1.css('background-color'),
+    background2: home_header_bg2.css('background-color')
+  }, {
+    selector: '#what_header',
+    background1: $('#what_header > .dummy-background-1').css('background-color'),
+    background2: $('#what_header > .dummy-background-2').css('background-color')
+  }, {
+    selector: '#who_header',
+    background1: $('#who_header > .dummy-background-1').css('background-color'),
+    background2: $('#who_header > .dummy-background-2').css('background-color')
+  }, {
+    selector: '#news_header',
+    background1: $('#news_header > .dummy-background-1').css('background-color'),
+    background2: $('#news_header > .dummy-background-2').css('background-color')
+  }, {
+    selector: '#partners_header',
+    background1: $('#partners_header > .dummy-background-1').css('background-color'),
+    background2: $('#partners_header > .dummy-background-2').css('background-color')
+  }, {
+    selector: '#contacts_header',
+    background1: $('#contacts_header > .dummy-background-1').css('background-color'),
+    background2: $('#contacts_header > .dummy-background-2').css('background-color')
+  }];
 
-		curMarker = -1;
-		for(var i = 0; i < headers.length; ++i) {
-			if( headerTop >= $(headers[i].selector).offset().top )
-				curMarker++;
-		}
-		$('#home_header > .dummy-background-1').css('background-color', headers[curMarker].background1);
-		$('#home_header > .dummy-background-2').css('background-color', headers[curMarker].background2);
-		
-	});
+  var home_header = $('#home_header');
+  var curMarker = -1;
+  for (var i = 0; i < headers.length; ++i) {
+    if (home_header.offset().top >= $(headers[i].selector).offset().top)
+      curMarker++;
+  }
 
-	// Adds animation for section transitions
-	$('a[href=#home], a[href=#what], a[href=#who], a[href=#news], a[href=#partners], a[href=#contacts]').click(function(event) {
-		var href = $(this).attr('href');
-		var top2scroll2 = (href == '#home' ? 0 : $(href + '_header').offset().top);
+  $(window).scroll(function () {
+    var headerTop = $('#home_header').offset().top;
 
-		window.stop();
+    curMarker = -1;
+    for (var i = 0; i < headers.length; ++i) {
+      if (headerTop >= $(headers[i].selector).offset().top)
+        curMarker++;
+    }
+    $('#home_header > .dummy-background-1').css('background-color', headers[curMarker].background1);
+    $('#home_header > .dummy-background-2').css('background-color', headers[curMarker].background2);
 
-		$('html, body').animate({
- 			scrollTop: top2scroll2+1,
-		}, 500, function () {
-			$('#home_header > .dummy-background-1').css('background-color', headers[curMarker].background1);
-			$('#home_header > .dummy-background-2').css('background-color', headers[curMarker].background2);
-		});
+  });
 
-		history.replaceState(null, '', href); // FIXME
+  // Adds animation for section transitions
+  $('a[href=#home], a[href=#what], a[href=#who], a[href=#news], a[href=#partners], a[href=#contacts]').click(function (event) {
+    var href = $(this).attr('href');
+    var top2scroll2 = (href == '#home' ? 0 : $(href + '_header').offset().top);
 
-		event.preventDefault();
-	});
+    window.stop();
 
-	var what_section = $('#what');
-	$('.expander', what_section).click(function(event) {
-		$('*').stop();
+    $('html, body').animate({
+      scrollTop: top2scroll2 + 1,
+    }, 500, function () {
+      $('#home_header > .dummy-background-1').css('background-color', headers[curMarker].background1);
+      $('#home_header > .dummy-background-2').css('background-color', headers[curMarker].background2);
+    });
 
-		$('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]', what_section)
-			.slideToggle('slow');
-	
-		if( $(this).children().text() == "+" ) {
-			$(this).children().text(">");
+    history.replaceState(null, '', href); // FIXME
 
-			$('.expander', what_section).not($(this)).each(function() {
-				if ($(this).children().text() == "+")
-					return;
+    event.preventDefault();
+  });
 
-				$(this).children().text("+");
+  var what_section = $('#what');
+  $('.expander', what_section).click(function (event) {
+    $('*').stop();
+
+    $('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]', what_section)
+      .slideToggle('slow');
+
+    if ($(this).children().text() == '+') {
+      $(this).children().text('>');
+
+      $('.expander', what_section).not($(this)).each(function () {
+        if ($(this).children().text() == '+')
+          return;
+
+        $(this).children().text('+');
 
 
-				$('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]', what_section)
-					.slideToggle('fast');
-			});
-		}
-		else {
-			$(this).children().text("+");
-		}
+        $('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]', what_section)
+          .slideToggle('fast');
+      });
+    } else {
+      $(this).children().text('+');
+    }
 
-		// history.replaceState(null, '', $(this).id()); // FIXME
-		event.preventDefault();
-	});
+    event.preventDefault();
+  });
 
-	var news_section = $('#news');
-	$('.expander', news_section).click(function(event) {
-		$('*').stop(); // TO REMOVE
-		
-		$('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]', news_section)
-			.slideToggle('slow');
-	
-		if( $(this).children().text() == "+" ) {
-			$(this).children().text(">");
+  var news_section = $('#news');
+  $('.expander', news_section).click(function (event) {
+    $('*').stop(); // TO REMOVE
 
-			$('.expander', news_section).not($(this)).each(function() {
-				if ($(this).children().text() == "+")
-					return;
+    $('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]', news_section)
+      .slideToggle('slow');
 
-				$(this).children().text("+");
+    if ($(this).children().text() == '+') {
+      $(this).children().text('>');
+
+      $('.expander', news_section).not($(this)).each(function () {
+        if ($(this).children().text() == '+')
+          return;
+
+        $(this).children().text('+');
 
 
-				$('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]', news_section)
-					.slideToggle('fast')
-			});
-		}
-		else {
-			$(this).children().text("+");
-		}
+        $('.expanded-content[data-expanded-content="' + $(this).attr('data-expander') + '"]', news_section)
+          .slideToggle('fast');
+      });
+    } else {
+      $(this).children().text('+');
+    }
 
-		// history.replaceState(null, '', $(this).id()); // FIXME
-		event.preventDefault();
-	});
+    // history.replaceState(null, '', $(this).id()); // FIXME
+    event.preventDefault();
+  });
 
-	$('#who .section-content img').mouseenter(function() {
-		$(this).addClass('highlighted');
-	}).mouseleave(function() {
-		$(this).removeClass('highlighted');
-	});
+  $('#who .section-content img').mouseenter(function () {
+    $(this).addClass('highlighted');
+  }).mouseleave(function () {
+    $(this).removeClass('highlighted');
+  });
 
+  var who_section = $('#who');
 
+  $('.selected-content[data-selected-content="1"]', who_section).show();
 
-
-	var who_section = $('#who');
-
-	$('.selected-content[data-selected-content="1"]', who_section).show();
-
-	$('.selector', who_section).click(function(event) {
-
-	
-		if( !$(this).hasClass('selected') ) {
-			$('*').stop(); // TO REMOVE
-
-			$('.selected-content[data-selected-content="' + $(this).attr('data-selector') + '"]', who_section)
-				.slideToggle('slow');
-
-			$(this).addClass('selected');
-
-			$('.selector', who_section).not($(this)).each(function() {
-				if ( !$(this).hasClass('selected') )
-					return;
-
-				!$(this).removeClass('selected');
+  $('.selector', who_section).click(function () {
 
 
-				$('.selected-content[data-selected-content="' + $(this).attr('data-selector') + '"]', who_section)
-					.slideToggle('fast');
-			});
-		}
-		else {
-			// $(this).removeClass('selected');
-		}
+    if (!$(this).hasClass('selected')) {
+      $('*').stop(); // TO REMOVE
 
-	});
+      $('.selected-content[data-selected-content="' + $(this).attr('data-selector') + '"]', who_section)
+        .slideToggle('slow');
 
-	$('body').mousedown(function(e) {
-		if( e.button == 1 )
-			return false;
-		e.preventDefault();
-	});
+      $(this).addClass('selected');
 
-	var map;
-	var center = new google.maps.LatLng(41.1493783, -8.606509599999981);
+      $('.selector', who_section).not($(this)).each(function () {
+        if (!$(this).hasClass('selected'))
+          return;
 
-	function initialize() {
-		var mapOptions = {
-			zoom: 15,
-			center: center,
-			mapTypeId: google.maps.MapTypeId.ROADMAP,
-		};
-
-		$('.content-map').css({opacity: 0.8});
-		map = new google.maps.Map($('.content-map')[0], mapOptions);
-
-		map.setOptions({scrollwheel: false});
-
-		var marker = new google.maps.Marker({
-			map: map,
-			draggable: false,
-			animation: google.maps.Animation.DROP,
-			position: center
-		});
-
-		google.maps.event.addListener(marker, 'click', function() {
-			map.setZoom(15);
-			map.setCenter(marker.getPosition());
-		});
-
-	}
-
-	google.maps.event.addDomListener(window, 'load', initialize);
+        $(this).removeClass('selected');
 
 
-	$('.content-map').mousedown(function() {
-		$(this).css({opacity: 1})
-		map.setOptions({scrollwheel: true});
-	}).mouseleave(function() {
-		$(this).css({opacity: 0.8})
-		map.setOptions({scrollwheel: false});
-	});
+        $('.selected-content[data-selected-content="' + $(this).attr('data-selector') + '"]', who_section)
+          .slideToggle('fast');
+      });
+    } else {
+      // $(this).removeClass('selected');
+    }
 
-	/*
+  });
+
+  $('body').mousedown(function (e) {
+    if (e.button == 1)
+      return false;
+    e.preventDefault();
+  });
+
+  var map;
+
+  var center = new google.maps.LatLng(41.1493783, -8.606509599999981);
+
+  function initialize() {
+    var mapOptions = {
+      zoom: 15,
+      center: center,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+    };
+
+    $('.content-map').css({
+      opacity: 0.8
+    });
+    map = new google.maps.Map($('.content-map')[0], mapOptions);
+
+    map.setOptions({
+      scrollwheel: false
+    });
+
+    var marker = new google.maps.Marker({
+      map: map,
+      draggable: false,
+      animation: google.maps.Animation.DROP,
+      position: center
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+      map.setZoom(15);
+      map.setCenter(marker.getPosition());
+    });
+
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+
+  $('.content-map').mousedown(function () {
+    $(this).css({
+      opacity: 1
+    });
+    map.setOptions({
+      scrollwheel: true
+    });
+  }).mouseleave(function () {
+    $(this).css({
+      opacity: 0.8
+    });
+    map.setOptions({
+      scrollwheel: false
+    });
+  });
+
+  /*
 
 	var context = document.getElementById('what_canvas').getContext('2d');
 	var x0 = 25, y0 = 10,
